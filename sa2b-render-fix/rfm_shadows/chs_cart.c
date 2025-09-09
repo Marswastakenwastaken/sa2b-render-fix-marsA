56b4205f6be5333341e50cdd667eb81234d4bf24
Mars <mars1273max@gmail.com>
2025-09-09 08:56:47 -0400

    Refactor cart modifier system.
    Changed how Render Fix loads its volume models by instead using `exParam`'s `kageObject` and replacing those with the respective volumes.

--- sa2b-render-fix/rfm_shadows/chs_cart.c
+++ sa2b-render-fix/rfm_shadows/chs_cart.c
@@ -1,23 +1,26 @@
-#include <samt/core.h>
+/********************************/
+/*  Includes                    */
+/********************************/
+/****** SAMT ************************************************************************************/
+#include <samt/core.h>              /* core                                                     */
 #include <samt/memory.h>
-#include <samt/writemem.h>
-#include <samt/writeop.h>
-#include <samt/funchook.h>
+#include <samt/writemem.h>          /* writedata                                                */
+#include <samt/writeop.h>           /* writecall                                                */
+#include <samt/funchook.h>          /* hooking                                                  */
 
-/** Ninja **/
-#include <samt/ninja/ninja.h>
+/****** Ninja ***********************************************************************************/
+#include <samt/ninja/ninja.h>       /* ninja                                                    */
 
-/** Source **/
+/****** Game ************************************************************************************/
 #include <samt/sonic/task.h>
 #include <samt/sonic/camera.h>
-#include <samt/sonic/njctrl.h>
-#include <samt/sonic/debug.h>
+#include <samt/sonic/njctrl.h>      /* control                                                  */
+#include <samt/sonic/cart/cartcar.h>
 
-/** Render Fix **/
-#include <rf_core.h>
-#include <rf_file.h>
+/****** Render Fix ******************************************************************************/
+#include <rf_core.h>                /* core                                                     */
+#include <rf_file.h>                /* files                                                    */
 #include <rf_draw.h>
-#include <rf_mdlutil.h>
 
 #define GET_CARTWK(_tp)     ((CARTWK*)_tp->mwp)
 
@@ -56,14 +59,13 @@ CARTWK;
 
 #define PlayerCartSelected      DATA_ARY(s8, 0x0174B021, [2])
 
-static NJS_CNK_OBJECT* CartObjectList[8][2];
-
 #define SQR(x)          ((x)*(x))
 
 static void
 cartDisplayerMod(task* tp)
 {
     CARTWK* const cwp = GET_CARTWK(tp);
+    CART_EXTRA_PARAM* exParam = GetDataDllAddr(CART_EXTRA_PARAM, "exParam");
 
     camcontwk* const camwk = cameraCurrentWork;
 
@@ -82,66 +84,39 @@ cartDisplayerMod(task* tp)
 
     OnControl3D(NJD_CONTROL_3D_SHADOW | NJD_CONTROL_3D_TRANS_MODIFIER);
 
-    bool alt;
-
     const NJS_POINT3* p_pos;
 
-    if ( cwp->is_player )
+    if (cwp->is_player)
     {
         const int chnum = PlayerCartSelected[cwp->player_num];
-
-        alt = (chnum < 0);
 
         p_pos = &cwp->pos;
     }
     else
     {
-        alt = false;
 
         p_pos = &tp->twp->pos;
     }
 
     njPushMatrixEx();
-
-    njTranslate(NULL, p_pos->x, p_pos->y + 0.01f - 2.0f, p_pos->z);
-
-    njRotateZ(NULL, cwp->ang.z);
-    njRotateX(NULL, cwp->ang.x);
-    njRotateY(NULL, cwp->ang.y + cwp->angSlide - 0x8000);
-
-    njRotateZ(NULL, cwp->ang2.z + cwp->ang3.z);
-    njRotateX(NULL, cwp->ang2.x + cwp->ang3.x);
-    njRotateY(NULL, cwp->ang2.y + cwp->ang3.y);
-
-    const NJS_CNK_OBJECT* const p_obj = CartObjectList[cwp->character][alt];
-
-    switch (cwp->character)
     {
-        case 0:
-        {
-            njRotateY(NULL, 0x4000);
-            njCnkModDrawObject(p_obj);
-            break;
-        }
-        case 1:
-        case 2: case 3: case 4:
-        case 5: case 6: case 7:
-        {
-            njCnkModDrawObject(p_obj);
-            break;
-        }
-        case 8:
-        {
-            njScale(NULL, 5.0f, 1.0f, 5.0f);
-            DrawBasicShadow();
-            break;
-        }
-    }
+        njTranslate(NULL, p_pos->x, p_pos->y + 0.01f - 2.0f, p_pos->z);
 
+        njRotateZ(NULL, cwp->ang.z);
+        njRotateX(NULL, cwp->ang.x);
+        njRotateY(NULL, cwp->ang.y + cwp->angSlide - 0x8000);
+
+        njRotateZ(NULL, cwp->ang2.z + cwp->ang3.z);
+        njRotateX(NULL, cwp->ang2.x + cwp->ang3.x);
+        njRotateY(NULL, cwp->ang2.y + cwp->ang3.y);
+
+        njCnkModDrawObject(exParam[cwp->character].kageObject);
+    }
     njPopMatrixEx();
 
     OffControl3D(NJD_CONTROL_3D_SHADOW | NJD_CONTROL_3D_TRANS_MODIFIER);
 }
+
 
 #define courseDisplayDisplayer      FUNC_PTR(void, __cdecl, (task*), 0x00623E10)
 
@@ -166,66 +141,23 @@ CHS_CartInit(void)
 
     FuncHook(HookInfoCourseDisplayDisplayer, courseDisplayDisplayer, courseDisplayDisplayerHook);
 
-    LOAD_SAMDL_LIST mdl_list[8][2] =
-    {
-        {
-            { .puPath = "cart/cart_tails_big_mod" },
-            { .puPath = nullptr },
-        },
-        {
-            { .puPath = "cart/cart_rouge_big_mod" },
-            { .puPath = nullptr },
-        },
-        {
-            { .puPath = "cart/cart_sonic_mod" },
-            { .puPath = "cart/cart_sonic_alt_mod" },
-        },
-        {
-            { .puPath = "cart/cart_knuckles_mod" },
-            { .puPath = "cart/cart_knuckles_alt_mod" },
-        },
-        {
-            { .puPath = "cart/cart_tails_mod" },
-            { .puPath = "cart/cart_chao_mod" },
-        },
-        {
-            { .puPath = "cart/cart_eggman_mod" },
-            { .puPath = "cart/cart_eggman_alt_mod" },
-        },
-        {
-            { .puPath = "cart/cart_shadow_mod" },
-            { .puPath = "cart/cart_shadow_alt_mod" },
-        },
-        {
-            { .puPath = "cart/cart_rouge_mod" },
-            { .puPath = "cart/cart_eggrobo_mod" },
-        },
-    };
+    CART_EXTRA_PARAM* exParam = GetDataDllAddr(CART_EXTRA_PARAM, "exParam");
+    CART_EXTRA_PARAM* exParamSpecial = GetDataDllAddr(CART_EXTRA_PARAM, "exParamSpecial");
 
-    RF_LoadChunkObjectList( (LOAD_SAMDL_LIST*) mdl_list, 8 * 2);
+    exParam[CART_CAR_TAILS_SPECIAL].kageObject = RF_ChunkLoadObjectFile("cart/cart_tails_big_mod");
+    exParam[CART_CAR_ROUGE_SPECIAL].kageObject = RF_ChunkLoadObjectFile("cart/cart_rouge_big_mod");
 
-    /** Models **/
-    CartObjectList[0][0] = mdl_list[0][0].pSamdl->pChunk;
-//  CartObjectList[0][1] = mdl_list[0][1].pSamdl->pChunk;
+    exParam[CART_CAR_SONIC].kageObject = RF_ChunkLoadObjectFile("cart/cart_sonic_mod");
+    exParam[CART_CAR_KNUCKLES].kageObject = RF_ChunkLoadObjectFile("cart/cart_knuckles_mod");
+    exParam[CART_CAR_TAILS].kageObject = RF_ChunkLoadObjectFile("cart/cart_tails_mod");
+    exParam[CART_CAR_EGGMAN].kageObject = RF_ChunkLoadObjectFile("cart/cart_eggman_mod");
+    exParam[CART_CAR_SHADOW].kageObject = RF_ChunkLoadObjectFile("cart/cart_rouge_mod");
+    exParam[CART_CAR_ROUGE].kageObject = RF_ChunkLoadObjectFile("cart/cart_eggman_mod");
 
-    CartObjectList[1][0] = mdl_list[1][0].pSamdl->pChunk;
-//  CartObjectList[1][1] = mdl_list[1][1].pSamdl->pChunk;
-
-    CartObjectList[2][0] = mdl_list[2][0].pSamdl->pChunk;
-    CartObjectList[2][1] = mdl_list[2][1].pSamdl->pChunk;
-
-    CartObjectList[3][0] = mdl_list[3][0].pSamdl->pChunk;
-    CartObjectList[3][1] = mdl_list[3][1].pSamdl->pChunk;
-
-    CartObjectList[4][0] = mdl_list[4][0].pSamdl->pChunk;
-    CartObjectList[4][1] = mdl_list[4][1].pSamdl->pChunk;
-
-    CartObjectList[5][0] = mdl_list[5][0].pSamdl->pChunk;
-    CartObjectList[5][1] = mdl_list[5][1].pSamdl->pChunk;
-
-    CartObjectList[6][0] = mdl_list[6][0].pSamdl->pChunk;
-    CartObjectList[6][1] = mdl_list[6][1].pSamdl->pChunk;
-
-    CartObjectList[7][0] = mdl_list[7][0].pSamdl->pChunk;
-    CartObjectList[7][1] = mdl_list[7][1].pSamdl->pChunk;
+    exParamSpecial[0].kageObject = RF_ChunkLoadObjectFile("cart/cart_sonic_alt_mod");
+    exParamSpecial[1].kageObject = RF_ChunkLoadObjectFile("cart/cart_knuckles_alt_mod");
+    exParamSpecial[2].kageObject = RF_ChunkLoadObjectFile("cart/cart_chao_mod");
+    exParamSpecial[3].kageObject = RF_ChunkLoadObjectFile("cart/cart_eggman_alt_mod");
+    exParamSpecial[4].kageObject = RF_ChunkLoadObjectFile("cart/cart_shadow_alt_mod");
+    exParamSpecial[5].kageObject = RF_ChunkLoadObjectFile("cart/cart_eggrobo_mod");
 }
